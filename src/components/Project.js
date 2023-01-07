7df870b8355d9480ff25deaff30f276d00878f13
tagopig <khoumanitaha23@gmail.com>
2023-01-07 18:26:26 -0500

    step-one-finished

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,12 @@
 
 
 export default function Project ({projectData}){
-
+    const style ={
+        backgroundImage: `url(${projectData.picture})`,
+    }
     return(
-        <a className="abc" href={projectData.liveLink} rel="noreferrer" target="_blank">
-            <img className="shit" src={projectData.picture} alt="tribute page" />
+        <a style={style} className="abc" href={projectData.liveLink} rel="noreferrer" target="_blank">
+            {/* <img className="shit" src={projectData.picture} alt="tribute page" /> */}
             <p className="project-tile"><span className="code">&lt;</span> {projectData.title} <span className="code">/&gt;</span></p>
         </a>
     )
