c8b64b9f557c9a6e447008c5b08d53ee700b5830
tagopig <khoumanitaha23@gmail.com>
2023-01-07 19:57:20 -0500

    dd

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,11 +8,11 @@ export default function Footer (){
             <p id="f1">Let's work together...</p>
             <p id="f2">How do you take your coffee?</p>
             <nav id="profile-link2">
-                <a rel="noreferrer" href="d" target="_blank" ><i className="fa-brands fa-facebook-square"></i>Facebook</a>
+                <a rel="noreferrer" href="i-don't-have-facebook-but-i-have-gmail-so..." target="_blank" ><i className="fa-brands fa-facebook-square"></i>Facebook</a>
                 <a rel="noreferrer" href="https://github.com/tagopig?tab=repositories" target="_blank" ><i className="fa-brands fa-github"></i>GitHub</a>
-                <a rel="noreferrer" href="d" target="_blank" ><i className="fa-brands fa-twitter"></i>Twitter</a>
-                <a rel="noreferrer" href="d" target="_blank" ><i className="fa-solid fa-at"></i>Send a mail</a>
-                <a rel="noreferrer" href="d" target="_blank" ><i className="fa-solid fa-mobile-screen-button"></i>Call me</a>
+                <a rel="noreferrer" href="i-don't-have-twiter-but-i-have-gmail-so..." target="_blank" ><i className="fa-brands fa-twitter"></i>Twitter</a>
+                <a  href="mailto:khoumanitaha23@gmail.com"  ><i className="fa-solid fa-at"></i>Send a mail</a>
+                <a href="tel:514 713 1023"  ><i className="fa-solid fa-mobile-screen-button"></i>Call me</a>
             </nav>
             </div>
         </footer>
